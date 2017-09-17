2aceea7e5e1eff9f39e2dc76ffd377f7de7816ae
Wanderson <wandersonwhcr@gmail.com>
2017-09-17 18:07:37 -0300

    Include christian holidays

--- lib/Checkdomain/Holiday/Provider/BR.php
+++ lib/Checkdomain/Holiday/Provider/BR.php
@@ -2,6 +2,9 @@
 
 namespace Checkdomain\Holiday\Provider;
 
+use DateInterval;
+use DateTimeImmutable;
+
 /**
  * Brazilian holiday provider
  */
@@ -12,6 +15,10 @@ class BR extends AbstractEaster
      */
     public function getHolidaysByYear($year)
     {
+        // Easter
+        $easterDates  = $this->getEasterDates($year);
+        $easterSunday = DateTimeImmutable::createFromMutable($easterDates['easterSunday']);
+
         /**
          * @see http://www.planalto.gov.br/ccivil_03/leis/L0662.htm
          * @see http://www.planalto.gov.br/ccivil_03/leis/l6802.htm
@@ -27,6 +34,10 @@ class BR extends AbstractEaster
             '11-02' => $this->createData('Finados'),
             '11-15' => $this->createData('Proclamação da República'),
             '12-25' => $this->createData('Natal'),
+            // National Variable
+            $easterSunday->sub(new DateInterval('P47D'))->format(self::DATE_FORMAT) => $this->createData('Carnaval'),
+            $easterSunday->sub(new DateInterval('P2D'))->format(self::DATE_FORMAT)  => $this->createData('Sexta-Feira Santa'),
+            $easterSunday->add(new DateInterval('P60D'))->format(self::DATE_FORMAT) => $this->createData('Corpus Christi'),
         );
 
         return $holidays;
--- tests/Checkdomain/Holiday/Provider/BRTest.php
+++ tests/Checkdomain/Holiday/Provider/BRTest.php
@@ -23,6 +23,7 @@ class BRTest extends AbstractTest
     public function dateProvider()
     {
         return array(
+            // National Fixed
             array('2017-01-01', null, array('name' => 'Confraternização Universal')),
             array('2017-04-21', null, array('name' => 'Tiradentes')),
             array('2017-05-01', null, array('name' => 'Dia do Trabalhador')),
@@ -31,6 +32,16 @@ class BRTest extends AbstractTest
             array('2017-11-02', null, array('name' => 'Finados')),
             array('2017-11-15', null, array('name' => 'Proclamação da República')),
             array('2017-12-25', null, array('name' => 'Natal')),
+            // National Variable
+            array('2016-02-09', null, array('name' => 'Carnaval')),
+            array('2017-02-28', null, array('name' => 'Carnaval')),
+            array('2018-02-13', null, array('name' => 'Carnaval')),
+            array('2016-03-25', null, array('name' => 'Sexta-Feira Santa')),
+            array('2017-04-14', null, array('name' => 'Sexta-Feira Santa')),
+            array('2018-03-30', null, array('name' => 'Sexta-Feira Santa')),
+            array('2016-05-26', null, array('name' => 'Corpus Christi')),
+            array('2017-06-15', null, array('name' => 'Corpus Christi')),
+            array('2018-05-31', null, array('name' => 'Corpus Christi')),
         );
     }
 }
