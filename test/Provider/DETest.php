88f2df0792a0dedd7740cbc1a14ee564341cd4e2
Andreas Lemke <andreas.lemke@tu-braunschweig.de>
2018-08-14 15:24:11 +0200

    Change Test for Reformationstag for STATE_SH

--- test/Provider/DETest.php
+++ test/Provider/DETest.php
@@ -35,7 +35,7 @@ class DETest extends AbstractTest
             array('2016-10-31', DE::STATE_BB, array('name' => 'Reformationstag')),
             array('2017-10-31', DE::STATE_SH, array('name' => 'Reformationstag')),
             array('2017-10-31', DE::STATE_BB, array('name' => 'Reformationstag')),
-            array('2018-10-31', DE::STATE_SH, null),
+            array('2018-10-31', DE::STATE_SH, array('name' => 'Reformationstag')),
             array('2018-11-21', DE::STATE_BB, null),
             array('2018-11-21', DE::STATE_SN, array('name' => 'Buß- und Bettag')),
             array('2018-10-31', DE::STATE_SH, null),
