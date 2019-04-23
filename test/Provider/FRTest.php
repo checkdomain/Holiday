ff044c10e993447b00a61e5f300068e9f78ad9bd
LIBERT Jérémy <libert.jeremy@gmail.com>
2019-04-23 11:12:45 +0200

    Deleted override function

--- test/Provider/FRTest.php
+++ test/Provider/FRTest.php
@@ -16,33 +16,6 @@ class FRTest extends AbstractTest
     }
 
     /**
-     * @param string $date
-     * @param string $state
-     * @param array  $expectation
-     *
-     * @dataProvider dateProvider
-     */
-    public function testHolidays($date, $state = null, array $expectation = null)
-    {
-        $date    = new \DateTime($date);
-        $holiday = $this->provider->getHolidayByDate($date, $state);
-
-        if ($expectation === null) {
-            $this->assertNull($holiday);
-        } else {
-            $this->assertNotNull($holiday, 'No Holiday found but assumed to find one on '.$date->format('Y-m-d'));
-            $this->assertEquals($date->format('d.m.Y'), $holiday->getDate()->format('d.m.Y'));
-
-            foreach ($expectation as $property => $expectedValue) {
-                $method = 'get'.ucfirst($property);
-                $value = $holiday->$method();
-
-                $this->assertEquals($expectedValue, $value);
-            }
-        }
-    }
-
-    /**
      * Provides some test dates and the expectation
      *
      * @return array
